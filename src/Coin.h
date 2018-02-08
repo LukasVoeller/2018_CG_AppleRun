0ed00fcedc79c57464fecdc7f704bfdc9daa8e9b
SandyCandy <sandratieben@gmail.com>
2018-02-08 08:09:06 +0100

    Coins entfernen

--- src/Coin.h
+++ src/Coin.h
@@ -2,27 +2,12 @@
 //  Coin.h
 //  CGXcode
 //
-//  Created by Sandra Tieben on 06.02.18.
+//  Created by Sandra Tieben on 07.02.18.
 //  Copyright © 2018 Philipp Lensing. All rights reserved.
 //
 
 #ifndef Coin_h
 #define Coin_h
 
-#include "Constants.h"
-#include <stdio.h>
-#include "Model.h"
-#include <string>
-
-class Coin : public Model
-{
-public:
-	Coin();
-	Coin(const char* ModelFile, bool FitSize=true, float scale=1.0);
-	void update(float dtime);
-	
-protected:
-
-};
 
 #endif /* Coin_h */
