d792a3878496a6d9222b45c727491e5397176035
Lukas Voeller <voeller.lukas@gmail.com>
2018-02-26 15:57:34 +0100

    Libpng

---
+++ src/LoadPNG.cpp
@@ -0,0 +1,9 @@
+//
+//  LoadPNG.cpp
+//  CGXcode
+//
+//  Created by Lukas Völler on 19.02.18.
+//  Copyright © 2018 Philipp Lensing. All rights reserved.
+//
+
+#include "LoadPNG.hpp"
---
+++ src/LoadPNG.hpp
@@ -0,0 +1,14 @@
+//
+//  LoadPNG.hpp
+//  CGXcode
+//
+//  Created by Lukas Völler on 19.02.18.
+//  Copyright © 2018 Philipp Lensing. All rights reserved.
+//
+
+#ifndef LoadPNG_hpp
+#define LoadPNG_hpp
+
+#include <stdio.h>
+
+#endif /* LoadPNG_hpp */
