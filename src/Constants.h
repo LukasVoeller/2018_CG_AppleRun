13c41e1c9540aad39a823bd18145a0912b2ce5b4
Lukas Voeller <voeller.lukas@gmail.com>
2018-03-04 21:30:24 +0100

    Sexy Level

--- src/Constants.h
+++ src/Constants.h
@@ -34,14 +34,14 @@
 //#define JUMPPOWER 20.0f
 
 // Physics Lukas'
-#define GRAVITY -5.0f
+#define GRAVITY -10.0f
 #define DOWNFORCE 0.0f
 #define TERRAIN_HEIGHT 0.0f
 
-#define RUNSPEED 40.0f 				// Normal speed
-#define ADDSPEED 5.0 				// Additional speed for running
-#define ROTATIONSPEED 4.0f
-#define JUMPPOWER 50.0f
+#define RUNSPEED 30.0f 				// Normal speed
+#define ADDSPEED 10.0f 				// Additional speed for running
+#define ROTATIONSPEED 5.0f
+#define JUMPPOWER 30.0f				// 30 ist gut
 
 #define ALLCOINS 2;
 
@@ -59,10 +59,10 @@
 // Helper
 #define DELTA 0.001
 
-#define BORDER_MIN_X -200.0f
-#define BORDER_MAX_X 200.0f
-#define BORDER_MIN_Z -200.0f
-#define BORDER_MAX_Z 200.0f
+#define BORDER_MIN_X -200.0f	//-116
+#define BORDER_MAX_X 200.0f		//87
+#define BORDER_MIN_Z -200.0f	//-135
+#define BORDER_MAX_Z 200.0f		//34
 
 //View Controls
 #define CAMERA_OFFSET_X 0.0f
